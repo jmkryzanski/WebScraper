1ab17114bc31a9fa44146f6ae5b533b3a82ea28e
DESKTOP-64UVUHA\cheny <chenyanglinq@gmail.com>
2021-09-19 10:28:23 -0400

    deploy to heroku

--- server/server.js
+++ server/server.js
@@ -28,10 +28,10 @@ app.get("/", async (req, res) => {
 
 // listen to port 3000 and start initial scraping immediately
 app.listen(process.env.PORT || 3000, async () => {
-  console.log("app is running on port 3000");
-  listOfEvents = await scrapEvents();
-  console.log(listOfEvents);
-  console.log(listOfEvents.length);
+  // console.log("app is running on port 3000");
+  // listOfEvents = await scrapEvents();
+  // console.log(listOfEvents);
+  // console.log(listOfEvents.length);
 });
 
 // Update list of events repeatly by doing new scrapes
